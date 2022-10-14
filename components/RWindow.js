004cf64cf5885ce433c343b582aece33981bc5ad
Maxim Soldatov <fomaxi@gmail.com>
2022-10-15 00:31:37 +0300

    implement resize window

--- components/RWindow.js
+++ components/RWindow.js
@@ -7,29 +7,41 @@ export default function RWindow({ title, position: [left, top], setPosition, siz
   const [isMove, setIsMove] = useState(false);
   const headerRef = useRef();
   const offsetRef = useRef([0, 0]);
+  const sizeRef = useRef([0, 0]);
 
   const onMove = useCallback((e) => {
     const [offsetLeft, offsetTop] = offsetRef.current;
     setPosition([e.clientX - offsetLeft, e.clientY - offsetTop]);
   }, [setPosition]);
 
-  const onMouseDown = useCallback((e) => {
+  const onStartMove = useCallback((e) => {
     e.preventDefault();
     setIsMove(true);
     offsetRef.current = [e.clientX - left, e.clientY - top];
     listenPointerMove(document, onMove, () => setIsMove(false));
   }, [setIsMove, onMove, left, top]);
 
+  const onResize = useCallback((e) => {
+    const [width, height] = sizeRef.current;
+    setSize([e.clientX - width, e.clientY - height]);
+  }, [setSize]);
+
+  const onStartResize = useCallback((e) => {
+    e.preventDefault();
+    sizeRef.current = [e.clientX - width, e.clientY - height];
+    listenPointerMove(document, onResize);
+  }, [onResize, width, height]);
+
   return (
     <div className={styles.container} style={{ left: left + 'px', top: top + 'px', width: width + 'px', height: height + 'px' }}>
-      <div className={clsx({ [styles.header]: true, [styles.move]: isMove })} ref={headerRef} onMouseDown={onMouseDown}>
+      <div className={clsx({ [styles.header]: true, [styles.move]: isMove })} ref={headerRef} onMouseDown={onStartMove}>
         <div className={styles.title}>{title}</div>
         <div className={styles.close}>&times;</div>
       </div>
       <div className={styles.content}>
         {children}
       </div>
-      <div className={styles.resize}></div>
+      <div className={styles.resize} onMouseDown={onStartResize}></div>
     </div>
   );
 }

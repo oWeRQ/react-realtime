d09dea1727b144129909d1cb7738a92d71a7bd60
Maxim Soldatov <fomaxi@gmail.com>
2022-10-15 16:02:03 +0300

    implement sync storage

--- functions/useStorage.js
+++ functions/useStorage.js
@@ -1,29 +1,37 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
-function storageGet(key, value) {
+function storageGet(key) {
   try {
     const jsonValue = localStorage.getItem(key);
     if (jsonValue) {
       return JSON.parse(jsonValue);
     }
   } catch (e) {}
-
-  storageSet(key, value);
-  return value;
 }
 
 function storageSet(key, value) {
   try {
     const jsonValue = JSON.stringify(value);
     localStorage.setItem(key, jsonValue);
-    return true;
-  } catch (e) {
-    return false;
-  }
+    return value;
+  } catch (e) {}
 }
 
 export default function useStorage(key, value) {
-  const [state, setState] = useState(storageGet(key, value));
+  const [state, setState] = useState(storageGet(key) ?? storageSet(key, value));
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === key) {
+        setState(storageGet(key));
+      }
+    };
+
+    window.addEventListener('storage', listener);
+    return () => {
+      window.removeEventListener('storage', listener);
+    };
+  }, [key, setState]);
 
   const setter = useCallback(value => {
     setState(value);

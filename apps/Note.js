e7baa549867331b67cdcdb095523e3f92466e5ba
Maxim Soldatov <fomaxi@gmail.com>
2022-10-16 04:17:27 +0300

    update styles

--- apps/Note.js
+++ apps/Note.js
@@ -2,6 +2,6 @@ import RTextArea from '../components/RTextArea';
 
 export default function Note({ data, onData }) {
   return (
-    <RTextArea value={data.value ?? ''} onChange={value => onData({ value })} monospace />
+    <RTextArea value={data.value ?? ''} onChange={value => onData({ value })} monospace style={{ background: '#ffff60' }} />
   );
 }

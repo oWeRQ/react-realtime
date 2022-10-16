15b3e44686c82394c892804ed627103d20721739
Maxim Soldatov <fomaxi@gmail.com>
2022-10-16 19:52:16 +0300

    update checkbox

--- components/RCheckbox.js
+++ components/RCheckbox.js
@@ -1,5 +1,12 @@
-export default function RCheckbox({ ...rest }) {
+import clsx from '../functions/clsx';
+import styles from './RCheckbox.module.css';
+
+export default function RCheckbox({ children, className, ...rest }) {
   return (
-    <input type="checkbox" {...rest} />
+    <label className={clsx(styles.container, className)}>
+      <input className={clsx(styles.input)} type="checkbox" {...rest} />
+      <span className={clsx(styles.indicator)}></span>
+      <span className={styles.text}>{children}</span>
+    </label>
   );
 }

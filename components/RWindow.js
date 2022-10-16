b6a31ff91799a60bb9c2efb84f921b392bdbd759
Maxim Soldatov <fomaxi@gmail.com>
2022-10-16 23:15:19 +0300

    fix touch move

--- components/RWindow.js
+++ components/RWindow.js
@@ -11,24 +11,28 @@ export default function RWindow({ onClose, onFocus, zIndex, active, title, posit
   const sizeRef = useRef([0, 0]);
 
   const onMove = useCallback((e) => {
+    const { clientX, clientY } = (e.targetTouches ? e.targetTouches[0] : e);
     const [offsetLeft, offsetTop] = offsetRef.current;
     setIsMove(true);
-    setPosition([e.clientX - offsetLeft, e.clientY - offsetTop]);
+    setPosition([clientX - offsetLeft, clientY - offsetTop]);
   }, [setPosition]);
 
   const onStartMove = useCallback((e) => {
-    offsetRef.current = [e.clientX - left, e.clientY - top];
+    const { clientX, clientY } = (e.targetTouches ? e.targetTouches[0] : e);
+    offsetRef.current = [clientX - left, clientY - top];
     listenPointerMove(document, onMove, () => setIsMove(false));
   }, [onMove, left, top]);
 
   const onResize = useCallback((e) => {
+    const { clientX, clientY } = (e.targetTouches ? e.targetTouches[0] : e);
     const [width, height] = sizeRef.current;
-    setSize([e.clientX - width, e.clientY - height]);
+    setSize([clientX - width, clientY - height]);
   }, [setSize]);
 
   const onStartResize = useCallback((e) => {
     e.preventDefault();
-    sizeRef.current = [e.clientX - width, e.clientY - height];
+    const { clientX, clientY } = (e.targetTouches ? e.targetTouches[0] : e);
+    sizeRef.current = [clientX - width, clientY - height];
     listenPointerMove(document, onResize);
   }, [onResize, width, height]);
 
@@ -42,14 +46,14 @@ export default function RWindow({ onClose, onFocus, zIndex, active, title, posit
 
   return (
     <div onMouseDown={onFocus} className={clsx(styles.container, { [styles.active]: active })} style={style}>
-      <div className={clsx(styles.header, { [styles.move]: isMove })} ref={headerRef} onMouseDown={onStartMove}>
+      <div className={clsx(styles.header, { [styles.move]: isMove })} ref={headerRef} onMouseDown={onStartMove} onTouchStart={onStartMove}>
         <div className={styles.title}>{title}</div>
         <RButton className={styles.close} onClick={onClose}>&times;</RButton>
       </div>
       <div className={styles.content}>
         {children}
       </div>
-      <div className={styles.resize} onMouseDown={onStartResize}></div>
+      <div className={styles.resize} onMouseDown={onStartResize} onTouchStart={onStartResize}></div>
     </div>
   );
 }
--- functions/listenPointerMove.js
+++ functions/listenPointerMove.js
@@ -11,12 +11,20 @@ export default function listenPointerMove(el, onMove, onEnd) {
 		el.addEventListener('mousemove', onMove, {passive: true});
 		el.addEventListener('mouseup', onEndWrap, {passive: true});
 		el.addEventListener('mouseleave', onEndWrap, {passive: true});
+
+		el.addEventListener('touchmove', onMove, {passive: true});
+		el.addEventListener('touchend', onEndWrap, {passive: true});
+		el.addEventListener('touchcancel', onEndWrap, {passive: true});
 	}
 
 	function detach() {
 		el.removeEventListener('mousemove', onMove);
 		el.removeEventListener('mouseup', onEndWrap);
 		el.removeEventListener('mouseleave', onEndWrap);
+
+		el.removeEventListener('touchmove', onMove);
+		el.removeEventListener('touchend', onEndWrap);
+		el.removeEventListener('touchcancel', onEndWrap);
 	}
 
 	attach();

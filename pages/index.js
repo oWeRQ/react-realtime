13403937632da921368304268db60ae85e9a616a
Maxim Soldatov <fomaxi@gmail.com>
2022-10-18 01:09:20 +0300

    rewrite root

--- pages/index.js
+++ pages/index.js
@@ -12,47 +12,58 @@ import uniqId from '../functions/uniqId';
 
 export default function Home() {
   const [state, dispatch] = useSocketReducer('/api/socket', windowsReducer);
-  const useAction = (type) => useCallback((payload) => dispatch({ type, payload }), [type]);
-
-  const addWindow = useAction('addWindow');
-  const updateWindow = useAction('updateWindow');
-  const closeWindow = useAction('closeWindow');
-  const focusWindow = useAction('focusWindow');
 
   const windows = state.windows ?? [];
   const activeWindow = getMax(windows, win => win.zIndex);
   const isActive = win => win.id === activeWindow?.id;
 
-  const openApp = (app) => {
+  const updateWindow = useCallback(payload => {
+    dispatch({ type: 'updateWindow', payload });
+  }, [dispatch]);
+
+  const setPosition = win => position => dispatch({ type: 'updateWindow', payload: { id: win.id, position } });
+  const setSize = win => size => dispatch({ type: 'updateWindow', payload: { id: win.id, size } });
+  const onClose = win => () => dispatch({ type: 'closeWindow', payload: { id: win.id } });
+  const onFocus = win => () => dispatch({ type: 'focusWindow', payload: { id: win.id } });
+
+  const openApp = app => {
     const id = uniqId();
     const [left, top] = activeWindow?.position || [0, 0];
 
-    addWindow({
-      id,
-      appId: app.id,
-      state: {},
-      title: app.name,
-      position: [left + 14, top + 14],
-      size: [300, 300],
+    dispatch({
+      type: 'addWindow',
+      payload: {
+        id,
+        appId: app.id,
+        state: {},
+        title: app.name,
+        position: [left + 14, top + 14],
+        size: [300, 300],
+      },
     });
-  }
+  };
 
   return (
     <RDesktop>
       {windows.map(win =>
         <RWindow
           key={win.id}
-          onClose={() => closeWindow({ id: win.id })}
-          onFocus={() => focusWindow({ id: win.id })}
-          position={win.position}
-          setPosition={position => updateWindow({ id: win.id, position })}
-          size={win.size}
-          setSize={size => updateWindow({ id: win.id, size })}
           title={win.title}
           zIndex={win.zIndex}
+          position={win.position}
+          size={win.size}
           active={isActive(win)}
+          onClose={onClose(win)}
+          onFocus={onFocus(win)}
+          setPosition={setPosition(win)}
+          setSize={setSize(win)}
         >
-          <RApp id={win.id} appId={win.appId} state={win.state} updateWindow={updateWindow} />
+          <RApp
+            id={win.id}
+            appId={win.appId}
+            state={win.state}
+            updateWindow={updateWindow}
+          />
         </RWindow>
       )}
       <RTaskBar>
@@ -60,7 +71,7 @@ export default function Home() {
         {windows.map(win =>
           <RButton
             key={win.id}
-            onClick={() => focusWindow(win)}
+            onClick={onFocus(win)}
             active={isActive(win)}
             bold={isActive(win)}
           >{win.title}</RButton>

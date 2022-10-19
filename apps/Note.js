083425ab3c82f4ba5e4a4da19b4e13aecca2daa2
Maxim Soldatov <fomaxi@gmail.com>
2022-10-19 20:42:31 +0300

    implement selection sync

--- apps/Note.js
+++ apps/Note.js
@@ -1,7 +1,26 @@
+import { useRef } from 'react';
 import RTextArea from '../components/RTextArea';
 
 export default function Note({ state, setState }) {
+  const ref = useRef();
+
+  const onChange = value => {
+    setState(state => ({ ...state, value }));
+  };
+
+  const onSelection = selection => {
+    setState(state => ({ ...state, selection }));
+  };
+
   return (
-    <RTextArea value={state.value ?? ''} onChange={value => setState({ value })} monospace style={{ background: '#ffff60' }} />
+    <RTextArea
+      ref={ref}
+      value={state.value ?? ''}
+      onChange={onChange}
+      onSelection={onSelection}
+      selection={state.selection}
+      monospace
+      style={{ background: '#ffff60' }}
+    />
   );
 }
--- components/RTextArea.js
+++ components/RTextArea.js
@@ -1,12 +1,25 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect } from 'react';
 import clsx from '../functions/clsx';
 import styles from './RTextArea.module.css';
 
-export default forwardRef(function RTextArea({ children, onChange, className, monospace, ...rest }, ref) {
+export default forwardRef(function RTextArea({ children, onChange, onSelection, selection, className, monospace, ...rest }, ref) {
+  useEffect(() => {
+    ref.current.setSelectionRange(...selection);
+  }, [ref, selection]);
+
+  const selectionHandler = e => {
+    onSelection?.([e.target.selectionStart, e.target.selectionEnd, e.target.selectionDirection]);
+  };
+
+  const changeHandler = e => {
+    onChange(e.target.value);
+  };
+
   return (
     <textarea
       ref={ref}
-      onChange={e => onChange(e.target.value)}
+      onSelect={selectionHandler}
+      onChange={changeHandler}
       className={clsx(styles.container, { [styles.monospace]: monospace }, className)}
       {...rest}
     >{children}</textarea>
